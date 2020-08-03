ead4f6c70c11ea335380717a1731d25d6afc9432
ncthuan <ncthuan.it@gmail.com>
2020-08-03 19:14:06 +0700

    debugged forward functions

--- model.py
+++ model.py
@@ -7,8 +7,8 @@ from math import sqrt
 
 
 class SSD300(nn.Module):
-    def __init__(self, n_classes, ):
-        super(SSDModel, self).__init__()
+    def __init__(self, n_classes):
+        super(SSD300, self).__init__()
         self.n_classes = n_classes
         
         # VGG16 conv layers
@@ -45,7 +45,7 @@ class SSD300(nn.Module):
         self.conv9_1 = nn.Conv2d(512, 128, kernel_size=1, padding=0)
         self.conv9_2 = nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1)  # 5x5
         
-        self.conv10_1 = nn.Conv2d(512, 128, kernel_size=1)
+        self.conv10_1 = nn.Conv2d(256, 128, kernel_size=1)
         self.conv10_2 = nn.Conv2d(128, 256, kernel_size=3)  # 3x3
         
         self.conv11_1 = nn.Conv2d(256, 128, kernel_size=1)
@@ -62,7 +62,9 @@ class SSD300(nn.Module):
         self.init_weights()
 
 
-    def init_weights():
+    def init_weights(self):
+        ''' Load pretrained VGG16 parameters for some first layers and initialize the rest
+        '''
         state_dict = self.state_dict()
         layer_names = list(state_dict.keys())
         
@@ -76,20 +78,20 @@ class SSD300(nn.Module):
             
         # Convert fc6, fc7 to convolutional layers, and subsample (by decimation) to sizes of conv6 and conv7
         # fc6
-        conv_fc6_weight = pretrained_state_dict['classifier.0.weight'].view(4096, 512, 7, 7)  # (4096, 512, 7, 7)
-        conv_fc6_bias = pretrained_state_dict['classifier.0.bias']  # (4096)
+        conv_fc6_weight = vgg16['classifier.0.weight'].view(4096, 512, 7, 7)  # (4096, 512, 7, 7)
+        conv_fc6_bias = vgg16['classifier.0.bias']  # (4096)
         state_dict['conv6.weight'] = decimate(conv_fc6_weight, m=[4, None, 3, 3])  # (1024, 512, 3, 3)
         state_dict['conv6.bias'] = decimate(conv_fc6_bias, m=[4])  # (1024)
         # fc7
-        conv_fc7_weight = pretrained_state_dict['classifier.3.weight'].view(4096, 4096, 1, 1)  # (4096, 4096, 1, 1)
-        conv_fc7_bias = pretrained_state_dict['classifier.3.bias']  # (4096)
+        conv_fc7_weight = vgg16['classifier.3.weight'].view(4096, 4096, 1, 1)  # (4096, 4096, 1, 1)
+        conv_fc7_bias = vgg16['classifier.3.bias']  # (4096)
         state_dict['conv7.weight'] = decimate(conv_fc7_weight, m=[4, 4, None, None])  # (1024, 1024, 1, 1)
         state_dict['conv7.bias'] = decimate(conv_fc7_bias, m=[4])  # (1024)
         
         # Init extra conv and clf layers
         for layer_name in layer_names[30:]:
             if layer_name[-4:] == 'bias':
-                nn.init.zero_(state_dict[layer_name])
+                nn.init.zeros_(state_dict[layer_name])
             elif layer_name[-6:] == 'weight':
                 nn.init.xavier_uniform_(state_dict[layer_name])
             else:
@@ -143,31 +145,33 @@ class SSD300(nn.Module):
         fm1 = x
 
         # Detection
+        box_size = 4 + self.n_classes  # each box has 25 values: 4 offset values and 21 class scores
+        #
         det_fm38 = F.relu(self.det_conv4_3(fm38))
-        det_fm38 = det_fm38.permute(0, 2, 3, 1).contiguous()  # (N, 38, 38, 4*(4+n_classes) )
-        det_fm38 = det_fm38.view(n, -1, 4+n_classes)  # (N, 5776, 4+n_classes), there are a total 5776 boxes on this feature map
+        det_fm38 = det_fm38.permute(0, 2, 3, 1).contiguous()  # (N, 38, 38, 4*box_size )
+        det_fm38 = det_fm38.view(n, -1, box_size)  # (N, 5776, box_size), there are a total 5776 boxes on this feature map
         
         det_fm19 = F.relu(self.det_conv7(fm19))
-        det_fm19 = det_fm19.permute(0, 2, 3, 1).contiguous()  # (N, 19, 19, 6*(4+n_classes) )
-        det_fm19 = det_fm19.view(n, -1, 4+n_classes)  # (N, 2166, 4+n_classes), there are a total 2166 boxes on this feature map
+        det_fm19 = det_fm19.permute(0, 2, 3, 1).contiguous()  # (N, 19, 19, 6*box_size )
+        det_fm19 = det_fm19.view(n, -1, box_size)  # (N, 2166, box_size), there are a total 2166 boxes on this feature map
         
         det_fm10 = F.relu(self.det_conv8_2(fm10))
-        det_fm10 = det_fm10.permute(0, 2, 3, 1).contiguous()  # (N, 10, 10, 6*(4+n_classes) )
-        det_fm10 = det_fm10.view(n, -1, 4+n_classes)  # (N, 600, 4+n_classes), there are a total 600 boxes on this feature map
+        det_fm10 = det_fm10.permute(0, 2, 3, 1).contiguous()  # (N, 10, 10, 6*box_size )
+        det_fm10 = det_fm10.view(n, -1, box_size)  # (N, 600, box_size), there are a total 600 boxes on this feature map
         
         det_fm5  = F.relu(self.det_conv9_2(fm5))
-        det_fm5 = det_fm5.permute(0, 2, 3, 1).contiguous()  # (N, 5, 5, 6*(4+n_classes) )
-        det_fm5 = det_fm5.view(n, -1, 4+n_classes)  # (N, 150, 4+n_classes), there are a total 150 boxes on this feature map
+        det_fm5 = det_fm5.permute(0, 2, 3, 1).contiguous()  # (N, 5, 5, 6*box_size )
+        det_fm5 = det_fm5.view(n, -1, box_size)  # (N, 150, box_size), there are a total 150 boxes on this feature map
         
         det_fm3 = F.relu(self.det_conv10_2(fm3))
-        det_fm3 = det_fm3.permute(0, 2, 3, 1).contiguous()  # (N, 3, 3, 4*(4+n_classes) )
-        det_fm3 = det_fm3.view(n, -1, 4+n_classes)  # (N, 36, 4+n_classes), there are a total 36 boxes on this feature map
+        det_fm3 = det_fm3.permute(0, 2, 3, 1).contiguous()  # (N, 3, 3, 4*box_size )
+        det_fm3 = det_fm3.view(n, -1, box_size)  # (N, 36, box_size), there are a total 36 boxes on this feature map
         
         det_fm1 = F.relu(self.det_conv11_2(fm1))
-        det_fm1 = det_fm1.permute(0, 2, 3, 1).contiguous()  # (N, 1, 1, 4*(4+n_classes) )
-        det_fm1 = det_fm1.view(n, -1, 4+n_classes)  # (N, 4, 4+n_classes), there are a total 4 boxes on this feature map
+        det_fm1 = det_fm1.permute(0, 2, 3, 1).contiguous()  # (N, 1, 1, 4*box_size )
+        det_fm1 = det_fm1.view(n, -1, box_size)  # (N, 4, box_size), there are a total 4 boxes on this feature map
         
-        detection = torch.cat([det_fm38, det_fm19, det_fm10, det_fm5, det_fm3, det_fm1], dim=1)  # (N, 8732, 4+n_classes)
+        detection = torch.cat([det_fm38, det_fm19, det_fm10, det_fm5, det_fm3, det_fm1], dim=1)  # (N, 8732, box_size)
         offsets, class_scores = torch.split(detection, [4,self.n_classes], dim=2)
         
         return offsets, class_scores
@@ -216,25 +220,25 @@ class SSD300(nn.Module):
                         prior_boxes.append([cx, cy, w, h])
                         
                     # additional prior box:
-                    if fmap > 1:
+                    if dim > 1:
                         additional_scale = sqrt(s * obj_scales[fmaps[k + 1]])
                     else:
                         additional_scale = 1
-                    prior_boxes.append(cx, cy, additional_scale, additional_scale)
+                    prior_boxes.append([cx, cy, additional_scale, additional_scale])
         
-        prior_boxes = torch.Tensor(prior_boxes, dtype=torch.float)
+        prior_boxes = torch.FloatTensor(prior_boxes)
         prior_boxes.clamp_(min=0, max=1)
         assert prior_boxes.shape == (8732,4)
         
         return prior_boxes
 
     
-    def detect_objects(self, predicted_locs, predicted_scores, min_score, max_overlap, top_k):
+    def detect_objects(self, predicted_offsets, predicted_scores, min_score, max_overlap, top_k):
         """
         Decode the 8732 locations and class scores (output of ths SSD300) to detect objects.
         For each class, perform Non-Maximum Suppression (NMS) on boxes that are above a minimum threshold.
         
-        :param predicted_locs: predicted locations/boxes w.r.t the 8732 prior boxes, a tensor of dimensions (N, 8732, 4)
+        :param predicted_offsets: predicted offsets w.r.t the 8732 prior boxes, (gcxgcy), a tensor of dimensions (N, 8732, 4)
         :param predicted_scores: class scores for each of the encoded locations/boxes, a tensor of dimensions (N, 8732, n_classes)
         :param min_score: minimum threshold for a box to be considered a match for a certain class
         :param max_overlap: maximum overlap two boxes can have so that the one with the lower score is not suppressed via NMS
@@ -256,29 +260,93 @@ class MultiBoxLoss(nn.Module):
         self.neg_pos_ratio = neg_pos_ratio
         self.alpha = alpha
         self.smooth_l1 = nn.L1Loss()
-        self.cross_entropy = nn.CrossEntropyLoss(reduce=False)
+        self.cross_entropy = nn.CrossEntropyLoss(reduction='none')
         
-    def forward(self, predicted_locs, predicted_scores, boxes, labels):
+    def forward(self, predicted_offsets, predicted_scores, boxes, labels):
         '''
-        :param predicted_locs: predicted locations/boxes w.r.t the 8732 prior boxes, a tensor of dimensions (N, 8732, 4)
+        :param predicted_offsets: predicted offsets w.r.t the 8732 prior boxes, (gcxgcy), a tensor of dimensions (N, 8732, 4)
         :param predicted_scores: class scores for each of the encoded locations/boxes, a tensor of dimensions (N, 8732, n_classes)
-        :param boxes: true  object bounding boxes in boundary coordinates, a list of N tensors (n_objects, 4)
-        :param labels: true object labels, a list of N tensors (n_objects, 1)
+        :param boxes: true  object bounding boxes in boundary coordinates, (xy), a list of N tensors (n_objects, 4)
+        :param labels: true object labels, a list of N tensors (n_objects,)
         :return: multibox loss, a scalar
         '''
+        N = predicted_offsets.shape[0]
+        n_priors = self.priors_cxcy.size(0)
+        n_classes = predicted_scores.size(2)
         
-        # matching boxes
+        truth_offsets = torch.zeros((N, n_priors, 4), dtype=torch.float)
+        truth_classes = torch.zeros((N, n_priors), dtype=torch.long)
         
+        # Matching ground truth boxes
+        # for each image
+        for i in range(N):
+            n_objects = labels[i].shape[0]
+            
+            overlap = find_jaccard_overlap(self.priors_xy, boxes[i]) #(n_priors, n_boxes)
+            
+            # for each prior, find the max iou and the coresponding object id
+            prior_iou, prior_obj = overlap.max(dim=1) #(n_priors)
+            
+            # for each object, find the most suited prior id
+            _, object_prior = overlap.max(dim=0) #(n_objects)
+            # for each object, assign its most suited prior with object id 
+            #for j in range(n_objects):
+            #    prior_obj[object_prior[j]] = j
+            prior_obj[object_prior] = torch.LongTensor(range(n_objects))
+            # for each object, assign its most suited prior with hight iou to ensure it qualifies the thresholding 
+            prior_iou[object_prior] = 1.
+            
+            # match bbox coordinates
+            boxes_xy = boxes[i][prior_obj] # (8732, 4)
+            
+            # match prior class
+            prior_class = labels[i][prior_obj]  # (8732)
+            # thresholding: assign prior with iou < threshold to the class 0: background
+            prior_class[prior_iou < self.threshold] = 0
+            
+            # save in the truth tensors
+            truth_offsets[i,:,:] = cxcy_to_gcxgcy(xy_to_cxcy(boxes_xy), self.priors_cxcy)
+            truth_classes[i,:] = prior_class
         
-        # bbox loss
+        # Now we have truth_offsets, truth_classes and predicted_offsets, predicted_scores, we can now calculate the loss
         
+        positive_priors = (truth_classes != 0) #(N,8732)
         
-        # classification loss
+        # Calculating loss = alpha*loc_loss + conf_loss
+        # loc_loss: localization loss
+        loc_loss = self.smooth_l1(predicted_offsets[positive_priors], truth_offsets[positive_priors])
         
+        # Confidence loss
+        full_conf_loss = self.cross_entropy(predicted_scores.view(-1, n_classes), truth_classes.view(-1)) #(N*n_priors)
+        full_conf_loss = full_conf_loss.view(N, n_priors)
+        # However there is a huge unbalance between positive and negative priors so we only take the loss of the hard negative priors
+
+        # Hard negative mining
+        n_positives = positive_priors.sum(dim=1)  # (N)
+        n_hard_negatives = self.neg_pos_ratio * n_positives  # (N)
         
+        conf_loss_hard_neg = 0
+        # accummulate conf_loss_hard_neg for each sample in batch
+        for i in range(N):
+            conf_loss_neg,_ = full_conf_loss[i][~positive_priors[i]].sort(dim=0, descending=True) # (1-n_positives)
+            conf_loss_hard_neg = conf_loss_hard_neg + conf_loss_neg[0:n_hard_negatives[i]].sum()
         
-        #
-        loss = 
-        
-        
-        return loss
+        conf_loss = full_conf_loss[positive_priors].sum() + conf_loss_hard_neg
+
+        return self.alpha * loc_loss + conf_loss
+
+
+if __name__ == "__main__":
+    MySSD300 = SSD300(n_classes = 21)
+    
+    loss_func = MultiBoxLoss(priors_cxcy = MySSD300.get_prior_boxes(), threshold=0.5, neg_pos_ratio=3, alpha=1.)
+    
+    predicted_offsets = torch.randn((2,8732,4))
+    predicted_scores = torch.randn((2,8732,21))
+    
+    boxes = [torch.Tensor([[0.1, 0.2, 0.3, 0.4]]), torch.Tensor([[0.1, 0.2, 0.3, 0.4]])]
+    labels = [torch.LongTensor([2]), torch.LongTensor([3])]
+    
+    loss = loss_func.forward(predicted_offsets, predicted_scores, boxes, labels)
+    
+    print(loss.item())
