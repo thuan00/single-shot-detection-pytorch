0efaca480cbb614bc790997045b5074533b724c6
ncthuan <ncthuan.it@gmail.com>
2020-08-07 02:31:09 +0700

    modify cal_mAP function

--- utils.py
+++ utils.py
@@ -152,7 +152,7 @@ def decimate(tensor, m):
     return tensor
 
 
-def calculate_mAP(det_boxes, det_labels, det_scores, true_boxes, true_labels, true_difficulties):
+def calculate_mAP_diff(det_boxes, det_labels, det_scores, true_boxes, true_labels, true_difficulties):
     """
     Calculate the Mean Average Precision (mAP) of detected objects.
     See https://medium.com/@jonathan_hui/map-mean-average-precision-for-object-detection-45c121a31173 for an explanation
@@ -262,6 +262,131 @@ def calculate_mAP(det_boxes, det_labels, det_scores, true_boxes, true_labels, tr
         cumul_precision = cumul_true_positives / (
                 cumul_true_positives + cumul_false_positives + 1e-10)  # (n_class_detections)
         cumul_recall = cumul_true_positives / n_easy_class_objects  # (n_class_detections)
+
+        # Find the mean of the maximum of the precisions corresponding to recalls above the threshold 't'
+        recall_thresholds = torch.arange(start=0, end=1.1, step=.1).tolist()  # (11)
+        precisions = torch.zeros((len(recall_thresholds)), dtype=torch.float).to(device)  # (11)
+        for i, t in enumerate(recall_thresholds):
+            recalls_above_t = cumul_recall >= t
+            if recalls_above_t.any():
+                precisions[i] = cumul_precision[recalls_above_t].max()
+            else:
+                precisions[i] = 0.
+        average_precisions[c - 1] = precisions.mean()  # c is in [1, n_classes - 1]
+
+    # Calculate Mean Average Precision (mAP)
+    mean_average_precision = average_precisions.mean().item()
+
+    # Keep class-wise average precisions in a dictionary
+    average_precisions = {rev_label_map[c + 1]: v for c, v in enumerate(average_precisions.tolist())}
+
+    return average_precisions, mean_average_precision
+
+
+def calculate_mAP(det_boxes, det_labels, det_scores, true_boxes, true_labels):
+    """
+    Calculate the Mean Average Precision (mAP) of detected objects.
+    See https://medium.com/@jonathan_hui/map-mean-average-precision-for-object-detection-45c121a31173 for an explanation
+    :param det_boxes: list of tensors, one tensor for each image containing detected objects' bounding boxes
+    :param det_labels: list of tensors, one tensor for each image containing detected objects' labels
+    :param det_scores: list of tensors, one tensor for each image containing detected objects' labels' scores
+    :param true_boxes: list of tensors, one tensor for each image containing actual objects' bounding boxes
+    :param true_labels: list of tensors, one tensor for each image containing actual objects' labels
+    :return: list of average precisions for all classes, mean average precision (mAP)
+    """
+    assert len(det_boxes) == len(det_labels) == len(det_scores) == len(true_boxes) == len(true_labels)  
+    n_classes = len(label_map)
+
+    # Store all (true) objects in a single continuous tensor while keeping track of the image it is from
+    true_images = list()
+    for i in range(len(true_labels)):
+        true_images.extend([i] * true_labels[i].size(0))
+    true_images = torch.LongTensor(true_images).to(
+        device)  # (n_objects), n_objects is the total no. of objects across all images
+    true_boxes = torch.cat(true_boxes, dim=0)  # (n_objects, 4)
+    true_labels = torch.cat(true_labels, dim=0)  # (n_objects)
+
+    assert true_images.size(0) == true_boxes.size(0) == true_labels.size(0)
+
+    # Store all detections in a single continuous tensor while keeping track of the image it is from
+    det_images = list()
+    for i in range(len(det_labels)):
+        det_images.extend([i] * det_labels[i].size(0))
+    det_images = torch.LongTensor(det_images).to(device)  # (n_detections)
+    det_boxes = torch.cat(det_boxes, dim=0)  # (n_detections, 4)
+    det_labels = torch.cat(det_labels, dim=0)  # (n_detections)
+    det_scores = torch.cat(det_scores, dim=0)  # (n_detections)
+
+    assert det_images.size(0) == det_boxes.size(0) == det_labels.size(0) == det_scores.size(0)
+
+    # Calculate APs for each class (except background)
+    average_precisions = torch.zeros((n_classes - 1), dtype=torch.float)  # (n_classes - 1)
+    for c in range(1, n_classes):
+        # Extract only objects with this class
+        true_class_images = true_images[true_labels == c]  # (n_class_objects)
+        true_class_boxes = true_boxes[true_labels == c]  # (n_class_objects, 4)
+        
+        n_class_objects = true_class_images.size(0)
+        
+        # Keep track of which true objects with this class have already been 'detected'
+        # So far, none
+        true_class_boxes_detected = torch.zeros((n_class_objects), dtype=torch.uint8).to(
+            device)  # (n_class_objects)
+
+        # Extract only detections with this class
+        det_class_images = det_images[det_labels == c]  # (n_class_detections)
+        det_class_boxes = det_boxes[det_labels == c]  # (n_class_detections, 4)
+        det_class_scores = det_scores[det_labels == c]  # (n_class_detections)
+        n_class_detections = det_class_boxes.size(0)
+        if n_class_detections == 0:
+            continue
+
+        # Sort detections in decreasing order of confidence/scores
+        det_class_scores, sort_ind = torch.sort(det_class_scores, dim=0, descending=True)  # (n_class_detections)
+        det_class_images = det_class_images[sort_ind]  # (n_class_detections)
+        det_class_boxes = det_class_boxes[sort_ind]  # (n_class_detections, 4)
+
+        # In the order of decreasing scores, check if true or false positive
+        true_positives = torch.zeros((n_class_detections), dtype=torch.float).to(device)  # (n_class_detections)
+        false_positives = torch.zeros((n_class_detections), dtype=torch.float).to(device)  # (n_class_detections)
+        for d in range(n_class_detections):
+            this_detection_box = det_class_boxes[d].unsqueeze(0)  # (1, 4)
+            this_image = det_class_images[d]  # (), scalar
+
+            # Find objects in the same image with this class, and whether they have been detected before
+            object_boxes = true_class_boxes[true_class_images == this_image]  # (n_class_objects_in_img)
+            # If no such object in this image, then the detection is a false positive
+            if object_boxes.size(0) == 0:
+                false_positives[d] = 1
+                continue
+
+            # Find maximum overlap of this detection with objects in this image of this class
+            overlaps = find_jaccard_overlap(this_detection_box, object_boxes)  # (1, n_class_objects_in_img)
+            max_overlap, ind = torch.max(overlaps.squeeze(0), dim=0)  # (), () - scalars
+
+            # 'ind' is the index of the object in these image-level tensors 'object_boxes'
+            # In the original class-level tensors 'true_class_boxes', etc., 'ind' corresponds to object with index...
+            original_ind = torch.LongTensor(range(true_class_boxes.size(0)))[true_class_images == this_image][ind]
+            # We need 'original_ind' to update 'true_class_boxes_detected'
+
+            # If the maximum overlap is greater than the threshold of 0.5, it's a match
+            if max_overlap.item() > 0.5:
+                # If this object has already not been detected, it's a true positive
+                if true_class_boxes_detected[original_ind] == 0:
+                    true_positives[d] = 1
+                    true_class_boxes_detected[original_ind] = 1  # this object has now been detected/accounted for
+                # Otherwise, it's a false positive (since this object is already accounted for)
+                else:
+                    false_positives[d] = 1
+            # Otherwise, the detection occurs in a different location than the actual object, and is a false positive
+            else:
+                false_positives[d] = 1
+
+        # Compute cumulative precision and recall at each detection in the order of decreasing scores
+        cumul_true_positives = torch.cumsum(true_positives, dim=0)  # (n_class_detections)
+        cumul_false_positives = torch.cumsum(false_positives, dim=0)  # (n_class_detections)
+        cumul_precision = cumul_true_positives / (cumul_true_positives + cumul_false_positives + 1e-10)  # (n_class_detections)
+        cumul_recall = cumul_true_positives / n_class_objects  # (n_class_detections)
 
         # Find the mean of the maximum of the precisions corresponding to recalls above the threshold 't'
         recall_thresholds = torch.arange(start=0, end=1.1, step=.1).tolist()  # (11)
