3ba3062cb6e237f78559cc7ef30c15006bd5878b
ncthuan <ncthuan.it@gmail.com>
2020-08-09 23:26:28 +0700

    using albumentations

--- aug.py
+++ aug.py
@@ -1,55 +1,176 @@
 import albumentations as A
 import numpy as np
 import cv2
-import torch
-from torchvision import transforms
-
+import random
 
 class SSDTransform(object):
-    def __init__(self, input_size=300):
+    def __init__(self, size=300):
         self.transform = A.Compose([
-            A.Resize(   ) ,
-            A.RandomCrop(width=, height=),
-        ], bbox_params=A.BboxParams(format='pascal_voc'))
+            A.Resize(height=size, width=size, interpolation=cv2.INTER_LINEAR),
+            A.Normalize(),
+        ], bbox_params=A.BboxParams(format='pascal_voc', label_fields=['labels'], min_area=50))
         
-        self.to_tensor = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-        ])
-        
-    def __call__(self, images, boxes, labels):
-        
-        
-        return images, boxes, labels
+    def __call__(self, image, boxes, labels):
+        # padding for slim images -> square
+        h, w = image.shape[0:2]
+        if h > w:
+            padding = random.randint(0, h-w)
+            padded_img = np.full((h,h,3), (123, 117, 104), dtype='uint8')
+            padded_img[:, padding:padding+w, :] = image
+            for b in range(len(boxes)):
+                x1, y1, x2, y2 = boxes[b]
+                boxes[b] = (x1+padding, y1, x2+padding, y2)
+            image = padded_img
+        # perform regular transform
+        transformed = self.transform(image=image, bboxes=boxes, labels=labels)
+        return transformed['image'], transformed['bboxes'], transformed['labels']
 
+    
 class SSDAugmentation(object):
-    def __init__(self, input_size=300):
-        self.augment = A.Compose([
-            A.RandomCrop(width=, height=),
+    def __init__(self, size=300):
+        self.random_crop = [None]*4
+        self.random_crop[0] = A.Compose([
+            A.RandomCrop(width=size, height=size),
+        ], bbox_params=A.BboxParams(format='pascal_voc', label_fields=['labels'], min_visibility=0.5))
+        
+        self.random_crop[1] = A.Compose([
+            A.RandomCrop(width=round(size*1.3), height=size),
+        ], bbox_params=A.BboxParams(format='pascal_voc', label_fields=['labels'], min_visibility=0.4))
+        
+        self.random_crop[2] = A.Compose([
+            A.RandomCrop(width=round(size*1.5), height=size),
+        ], bbox_params=A.BboxParams(format='pascal_voc', label_fields=['labels'], min_visibility=0.3))
+        
+        self.random_crop[3] = A.Compose([
+            A.RandomCrop(width=round(size*1.2), height=round(size*1.2)),
+        ], bbox_params=A.BboxParams(format='pascal_voc', label_fields=['labels'], min_visibility=0.3))
+        
+        self.final_augment = A.Compose([
             A.HorizontalFlip(p=0.5),
             A.RandomBrightnessContrast(p=0.5),
-            A.RandomGamma(p=0.2),
-            A.CLAHE(p=0.2),  
-        ], bbox_params=A.BboxParams(format='coco'))
+            A.HueSaturationValue(p=0.5),
+            A.GaussNoise(var_limit=12.0, p=0.5),
+        ], bbox_params=A.BboxParams(format='pascal_voc', label_fields=['labels']))
         
-        self.transform = SSDTransform(input_size)
+    def __call__(self, image, boxes, labels, no_crop_pad=False):
+        p = random.random()
+        if no_crop_pad: p = 0.3 #avoid crop and pad, just perform regular augmentations
         
-    def __call__(self, images, boxes, labels):
-        
-
-        return 
+        #50% Random pad
+        if p > 0.5: 
+            image, boxes = self.random_pad(image, boxes)
+            
+        #25% Random crop
+        if p < 0.25:
+            random_crop = self.random_crop[random.randint(0, len(self.random_crop)-1)]
+            target_h, target_w = random_crop.transforms[0].height, random_crop.transforms[0].width
+            h, w = image.shape[0:2]
+            if h < target_h or w < target_w: # If requested crop size larger than the image size, pad
+                image, boxes = self.pad(image, boxes, target_h, target_w)
+            # ensure there is at least one obj in the image
+            n_iter = 0
+            while n_iter < 5:
+                cropped = random_crop(image=image.copy(), bboxes=boxes.copy(), labels=labels.copy())
+                n_iter += 1
+                if len(cropped['bboxes']) > 0: 
+                    image, boxes, labels = cropped['image'], cropped['bboxes'], cropped['labels']
+                    break
+                    
+        #25% no crop, no pad, just regular augmentations
+        augmented = self.final_augment(image=image, bboxes=boxes, labels=labels)
+        return augmented['image'], augmented['bboxes'], augmented['labels']
     
+    def random_pad(self, image, boxes):
+        ''' Expand the image and fill the border with ImageNet mean
+            If the image is slim, also do padding so that it is square
+        '''
+        h, w = image.shape[0:2]
+        scale = random.uniform(1.1,2.2) - 1
+        padding_h = round(h * scale)
+        padding_w = (h+padding_h - w) if (h > w) else round(w * scale)
+        i = round(random.random()*padding_h)
+        j = round(random.random()*padding_w)
 
+        padded_img = np.full((h+padding_h, w+padding_w, 3), (123, 117, 104), dtype='uint8')
+        padded_img[i:i+h, j:j+w, :] = image
+        
+        for b in range(len(boxes)):
+            x1, y1, x2, y2 = boxes[b]
+            boxes[b] = (x1+j, y1+i, x2+j, y2+i)
+
+        return padded_img, boxes
+    
+    def pad(self, image, boxes, target_h, target_w):
+        ''' equally padding all sides so that the image's shape >= the target shape'''
+        h, w = image.shape[0:2]
+        padding_h = max(0, target_h - h)
+        padding_w = max(0, target_w - w)
+        i = round(padding_h/2)
+        j = round(padding_w/2)
+
+        padded_img = np.full((h+padding_h, w+padding_w, 3), (123, 117, 104), dtype='uint8')
+        padded_img[i:i+h, j:j+w, :] = image
+        
+        for b in range(len(boxes)):
+            x1, y1, x2, y2 = boxes[b]
+            boxes[b] = (x1+j, y1+i, x2+j, y2+i)
+        
+        return padded_img, boxes
+
+    
+def visualize_bboxes(img, bboxes, thickness=1):
+    """Visualizes a single bounding box on the image"""
+    for bbox in bboxes:
+        x_min, y_min, x_max, y_max = bbox
+        x_min, y_min, x_max, y_max = round(x_min), round(y_min), round(x_max), round(y_max)
+        cv2.rectangle(img, (x_min, y_min), (x_max, y_max), color=(0,255,0), thickness=thickness)
+    return cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 
 if __name__ == "__main__":
-    img = cv2.imread("id.png")
+    from utils import parse_annotation
+    '''
+    
+    img_path = 'datasets/s/2008_000424.jpg'
+    anno_path = 'datasets/s/2008_000424.xml'
+    
+    img = cv2.imread(img_path)
+    #img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    target = parse_annotation(anno_path)
+    
+    img = cv2.imread(img_path)
     imgs = []
     for i in range(100):
         imgs.append(img)
         
-    transform = SSDAugmentation()
+    augment = SSDAugmentation(size=300)
+    transform = SSDTransform(size=300)
+    
     newDatas = transform(imgs)
     print(len(newDatas))
 
     for i, img in enumerate(newDatas):
         cv2.imwrite("__"+str(i)+".png", img)
+        
+    '''
+    
+    img_path = 'datasets/s/2008_000424.jpg'
+    anno_path = 'datasets/s/2008_000424.xml'
+    
+    img = cv2.imread(img_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    target = parse_annotation(anno_path)
+    
+    transform = A.Compose([
+        #A.RandomCropNearBBox(max_part_shift=0.3, p=1.0)
+        #A.Resize(height=300, width=300, interpolation=cv2.INTER_LINEAR)
+        A.RandomCrop(width=300, height=300, p=0.5)
+    ], bbox_params=A.BboxParams(format='pascal_voc', label_fields=['labels'], min_visibility=0.25, min_area=200))
+    
+    transformed = transform(image=img, bboxes=target['boxes'], labels=target['labels'], cropping_bbox=target['boxes'][2])
+    
+    #breakpoint()
+    img = visualize_bboxes(transformed['image'], transformed['bboxes'])
+    
+    cv2.imshow('img', img)
+    cv2.waitKey()
+    
